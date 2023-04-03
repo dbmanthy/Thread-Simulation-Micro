3e5f1e4fa528502c79934df174c8602cc7745814
HueManson <HueManson@users.noreply.github.com>
2023-04-02 20:05:00 -0700

    Menus complete!
    project done! Now to uplaod to uplaod to github pages
    +
    any additions haven't thought of yet

--- ThreadSimulation/Assets/Scripts/MenuManager.cs
+++ ThreadSimulation/Assets/Scripts/MenuManager.cs
@@ -14,10 +14,26 @@ public class MenuManager : MonoBehaviour
     public Slider yDimension;
     public Slider xSpacing;
     public Slider ySpacing;
+    public Slider meshLocking;
     public Text xDimensionText;
     public Text yDimensionText;
     public Text xSpacingText;
     public Text ySpacingText;
+    public Text meshLockingText;
+
+    [Header("Settings Settings")]
+    public Slider pointRadius;
+    public Slider lineThickness;
+    public Slider gravity;
+    public Slider friction;
+    public Slider bounceLoss;
+    public Slider regidity;
+    public Text pointRadiusText;
+    public Text lineThicknessText;
+    public Text gravityText;
+    public Text frictionText;
+    public Text bounceLossText;
+    public Text regidityText;
 
 
     VerletSimulation simulator;
@@ -35,6 +51,17 @@ public class MenuManager : MonoBehaviour
             yDimensionText.text = "y dimension: " + yDimension.value;
             xSpacingText.text = "x spacing: " + xSpacing.value;
             ySpacingText.text = "y spacing: " + ySpacing.value;
+            meshLockingText.text = "mesh spacing" + meshLocking.value;
+        }
+
+        if(SettinMenu.activeSelf)
+        {
+            pointRadiusText.text = "point radius: " + pointRadius.value;
+            lineThicknessText.text = "line thickness: " + lineThickness.value;
+            gravityText.text = "gravity: " + gravity.value;
+            frictionText.text = "friction: " + friction.value;
+            bounceLossText.text = "bounce loss: " + bounceLoss.value;
+            regidityText.text = "regidity: " + regidity.value;
         }
         
     }
@@ -69,6 +96,11 @@ public class MenuManager : MonoBehaviour
 
     public void SetSettings()
     {
-
+        simulator.pointRadius = pointRadius.value;
+        simulator.lineThickness = lineThickness.value;
+        simulator.gravity = gravity.value;
+        simulator.friciton = friction.value;
+        simulator.bounceLoss = bounceLoss.value;
+        simulator.regidity = regidity.value;
     }
 }
