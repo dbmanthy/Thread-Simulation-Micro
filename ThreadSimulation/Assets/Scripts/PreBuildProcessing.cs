7685a1ae6c34e0359d03c74a6159f23a6bf83b61
HueManson <HueManson@users.noreply.github.com>
2023-04-05 16:26:24 -0700

    Build work around
    hoping sretch issue is computer limitation not programming but -- wishful thinking

--- ThreadSimulation/Assets/Scripts/PreBuildProcessing.cs
+++ ThreadSimulation/Assets/Scripts/PreBuildProcessing.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+//Thank you https://answers.unity.com/questions/1893328/getting-systemcomponentmodelwin32exception-2-no-su.html
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
-public class WebGlBuildSalvage : MonoBehaviour
+public class PreBuildProcessing : IPreprocessBuildWithReport
 {
-    // Start is called before the first frame update
-    void Start()
+    public int callbackOrder => 1;
+    public void OnPreprocessBuild(BuildReport report)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        System.Environment.SetEnvironmentVariable("EMSDK_PYTHON", "/Library/Frameworks/Python.framework/Versions/2.7/bin/python");
     }
 }
+#endif
